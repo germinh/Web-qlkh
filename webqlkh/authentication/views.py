b6b76b7fd6edc3972dc154140d7303f2d2a530cf
germinh <144155765+germinh@users.noreply.github.com>
2023-10-26 16:56:30 +0700

    update5pm 26/10

--- webqlkh/authentication/views.py
+++ webqlkh/authentication/views.py
@@ -1,6 +1,8 @@
 
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def home(request):
@@ -10,6 +12,24 @@ def signin(request):
     return render(request, "authentication/signin.html")
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        sdt = request.POST['sdt']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create(username,email,sdt,pass1,pass2)
+
+        myuser.save()
+
+        messages.success(request, "Đăng ký thành công")
+
+        return redirect('signin')
+
+
+
+
     return render(request, "authentication/signup.html")
 
 def forgotpass(request):
